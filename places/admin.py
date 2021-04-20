e8fe1971c46937a474e1cb5fea18db0d626b2900
rkinwork <rkinwork@gmail.com>
2021-04-20 19:28:09 +0300

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import Place
+from .models import Place, Image
 
 
 # Register your models here.
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
     pass
--- places/models.py
+++ places/models.py
@@ -6,11 +6,47 @@ from django.db import models
 class Place(models.Model):
     """Model representing place of interest"""
     title = models.CharField(max_length=200,
-                             help_text="Название точки")
-    description_short = models.TextField("Короткое описание ", help_text="Короткое описание точки")
-    description_long = models.TextField("Полное описание", help_text="Полное описание точки")
+                             help_text="Название точки",
+                             )
+    description_short = models.TextField("Короткое описание ",
+                                         help_text="Короткое описание точки",
+                                         )
+    description_long = models.TextField("Полное описание",
+                                        help_text="Полное описание точки",
+                                        )
     lat = models.FloatField('Широта')
     long = models.FloatField('Долгота')
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    """Model representing places images"""
+    title = models.CharField('Имя картинки',
+                             max_length=200,
+                             null=True,
+                             blank=True,
+                             help_text="Название картинки",
+                             )
+
+    place = models.ForeignKey(Place,
+                              verbose_name='Место',
+                              related_name='images',
+                              on_delete=models.CASCADE,
+                              help_text='К какому месту отнести картинку'
+                              )
+
+    sorting_priority = models.PositiveSmallIntegerField('Сортировка',
+                                                        default=1,
+                                                        blank=True,
+                                                        help_text='приоритет сортировки, по умолчанию 1',
+                                                        )
+
+    image = models.ImageField(help_text='Изображение для модели')
+
+    class Meta:
+        ordering = ['sorting_priority']
+
+    def __str__(self):
+        return f'{self.sorting_priority} {self.place.title}'
