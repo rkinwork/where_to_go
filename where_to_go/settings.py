306e676bcfb29d50087b7c90020906d78332ebb5
rkinwork <rkinwork@gmail.com>
2021-05-31 09:38:33 +0300

    Move settings to env variables

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import environs
+
+env = environs.Env()
+env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w6)nbfr+474-omwgol#7+$^)t8pysyhu8+fl8#ol5_gplz2rbl'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
 # Application definition
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = env.str('TIME_ZONE', default='UTC')
 
 USE_I18N = True
 
@@ -118,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = env.str("SECRET_KEY", default='')
+
 STATICFILES_DIRS = [
     BASE_DIR.joinpath('static').as_posix()
 ]
@@ -128,3 +135,8 @@ MEDIA_ROOT = BASE_DIR.joinpath('media').as_posix()
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+SECURE_HSTS_SECONDS = env.int('SECURE_HSTS_SECONDS', default=None)
+SECURE_SSL_REDIRECT = env.bool('SECURE_SSL_REDIRECT', default=False)
+SESSION_COOKIE_SECURE = env.bool('SESSION_COOKIE_SECURE', default=False)
+CSRF_COOKIE_SECURE = env.bool('CSRF_COOKIE_SECURE', default=False)
